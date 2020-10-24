4a4bedf6ee61477d82fa1fec7f3ffe456249ca36
leopetkovic93 <leopetkovic93@gmail.com>
2020-10-24 23:43:21 +0200

    testing pull req

--- Hostel.Persistance/DependencyInjection.cs
+++ Hostel.Persistance/DependencyInjection.cs
@@ -12,7 +12,7 @@ namespace Hostel.Persistance
     {
         public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration)
         {
-            string connection = configuration["ConnectionStrings:HostelConnectionV"];
+            string connection = configuration["ConnectionStrings:HostelConnectionN"];
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddDbContext<HostelContext>(_ => _.UseSqlServer(connection).EnableSensitiveDataLogging());
 
